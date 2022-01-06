abd4872cd1138716bf41530dcd33565feeedab09
mde-figu <markusfwirz@gmail.com>
2022-01-05 22:21:44 -0300

    chore: refactor

--- philo/source/main.c
+++ philo/source/main.c
@@ -1,45 +1,48 @@
 #include "../includes/philo.h"
 
-int	validate_args(int argc)
+static int	validate_args(int argc)
 {
 	if (argc < 5 || argc > 6)
 	{
 		printf("Error: wrong number of arguments.\n");
-		return (0);
+		return (1);
 	}
-	return (1);
+	return (0);
 }
 
-int	entry_args_check(int argc, char *argv[])
+static int	entry_args_check(int argc, char *argv[])
 {
 	int i;
 	long int temp;
 
 	i = 1;
 	temp = 0;
+	argc--;
 	while (i <= argc)
 	{
 		if (!ft_isdigit(*argv[i]))
 		{
 			printf("Error: arguments must be all digits.\n");
-			return(0);
+			return(1);
 		}
 		temp = ft_atoi(argv[i]);
 		if (!temp)
 		{
 			printf("Error: arguments must be integers.\n");
-			return(0);
+			return(1);
 		}
 		i++;
 	}
-	return (1);
+	return (0);
 }
 
 int	main(int argc, char *argv[])
 {
-	validate_args(argc);
-	argc--;
-	entry_args_check(argc, argv);
+	if (validate_args(argc) != 0)
+		return(1);
+	//argc--;
+	if (entry_args_check(argc, argv) != 0)
+		return(1);
 	printf("Deu bom :D\n");
-	return(1);
+	return(0);
 }
